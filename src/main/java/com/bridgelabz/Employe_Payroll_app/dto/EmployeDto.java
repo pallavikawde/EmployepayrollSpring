386fd898bf3f62ac645b7fd3f06408df6220437e
yourpallavikkaawdegit config --global user.password your <pallavi.jagtap512@gmail.com>
2021-10-20 16:38:17 +0530

    Added:employePayrollApp

---
+++ src/main/java/com/bridgelabz/Employe_Payroll_app/dto/EmployeDto.java
@@ -0,0 +1,15 @@
+package com.bridgelabz.Employe_Payroll_app.dto;
+
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+
+@Data
+public class EmployeDto {
+    private String name;
+    private double salary;
+    private String id;
+    private String gender;
+
+
+}
